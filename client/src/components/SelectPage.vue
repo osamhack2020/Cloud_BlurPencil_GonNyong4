3f71afed45f780c55500df7690bb0404a6d77068
배성준 <clcc001@naver.com>
2020-10-13 22:48:17 +0000

    ADD Userinfoin navbar

--- client/src/components/SelectPage.vue
+++ client/src/components/SelectPage.vue
@@ -1,29 +1,37 @@
 <template>
   <div class="sidenav">
-    <div v-for="item in items" :key="item.link_to" class = "sidemenu">
-		<template v-if="item.link_to !== ''">
-			<router-link :to="item.link_to">{{item.title}}</router-link>
-		</template>
-		<template v-else>
-			<button @click="clickMenuBtn($event)" class="dropdown-btn">{{item.title}}</button>
-			<div class="dropdown-container">
-				<div v-for="subItem in item.sub_menu" :key="subItem.link_to">
-			<router-link :to="subItem.link_to">{{subItem.title}}</router-link>
-				</div>
-			</div>
-		</template>
+	<div class = "userwrap">
+		<p class = "userinfo" v-html = "userinfo"></p>
 	</div>
-	<template v-if = "isUser">
+	<div class = "menuwrap">
+		<div v-if = "isUser" class = "sidemenu">
 		<!-- <router-link v-on:click.native = "logout" to="/">Logout</router-link> -->
-		<a href="/" v-on:click = "logout">Logout</a>
-	</template>
-	<template v-else>
-		<router-link to="/login">Login</router-link>
-	</template>
+			<a href="/" v-on:click = "logout">Logout</a>
+		</div>
+		<div v-else class = "sidemenu">
+			<router-link to="/login">Login</router-link>
+		</div>
+		<div v-for="item in items" :key="item.link_to" class = "sidemenu">
+			<template v-if="item.link_to !== ''">
+				<router-link :to="item.link_to">{{item.title}}</router-link>
+			</template>
+			<template v-else>
+				<button @click="clickMenuBtn($event)" class="dropdown-btn">{{item.title}}</button>
+				<div class="dropdown-container">
+					<div v-for="subItem in item.sub_menu" :key="subItem.link_to">
+				<router-link :to="subItem.link_to">{{subItem.title}}</router-link>
+					</div>
+				</div>
+			</template>
+		</div>
+	</div>
+    
+	
   </div>
 </template>
 
 <script>
+
   const menuList = [
     { title: 'Home', link_to: '/'},
     { title: 'Upload', link_to: '/upload'},
@@ -40,6 +48,7 @@
     data(){
 		return{
 			items : menuList,
+			userid : '',
 		}
     },
     methods: {
@@ -58,17 +67,39 @@
 		isUser(){
 			var userid = sessionStorage.getItem("userid");
 			if(userid){
-				return true
+				return true;
 			}
 			return false
+		},
+		userinfo(){
+			var userid = sessionStorage.getItem("userid");
+			if(userid){
+				return userid+'<br>님 환영합니다.'
+			}
+			else
+				return '로그인이 <br>필요합니다.';
 		}
-		
 	}
   }
 
 </script>
 
 <style>
+	.userwrap{
+		background-color : #796943;
+		width : 6vw;
+		height : 14vh;
+		margin : 0 auto;
+		border-radius : 8px;
+		opacity: 0.6;
+	}
+	.userinfo{
+		color : white;
+		padding-top : 3vh;
+	}
+	.menuwrap{
+		padding-top : 2.2vh;
+	}
 	.sidenav {
 		height: 100%;
 		width: 10%;
@@ -98,85 +129,21 @@
 		outline: none;
 	}
 
-	/* When you mouse over the navigation links, change their color		*/
 	.sidenav a:hover {
 		color: #f1f1f1;
 	}
-	/* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
 	@media screen and (max-height: 450px) {
 		.sidenav {padding-top: 15px;}
 		.sidenav a {font-size: 18px;}
 	}
 
-	/* On mouse-over */
 	.sidenav a:hover, .dropdown-btn:hover {
 		color: #f1f1f1;
 	}
-	/* Add an active class to the active dropdown button */
-	.active {
-		background-color: green;
-		color: white;
-	}
-
-	/* Dropdown container (hidden by default). Optional: add a lighter background color and some left padding to change the design of the dropdown content */
 	.dropdown-container {
 		display: none;
 		background-color: #5eaf13;
 		padding-left: 8px;
 	}
 
-	/* Optional: Style the caret down icon */
-	.fa-caret-down {
-		float: right;
-		padding-right: 8px;
-	}
 </style>
-
-
-<!-- <template>
-  
-	<div>
-		<b-navbar toggleable="lg" type="dark" variant="info">
-		<b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-		<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-		<b-collapse id="nav-collapse" is-nav>
-			<b-nav>
-				<b-nav-item><router-link to="/"><p class ="listyle">Home</p></router-link></b-nav-item>
-				<b-nav-item><router-link to="/upload"><p class ="listyle">Upload</p></router-link></b-nav-item>
-				<b-nav-item><router-link to="/login"><p class ="listyle">Login</p></router-link></b-nav-item>
-			</b-nav>
-
-			<b-navbar-nav class="ml-auto">
-				<b-nav-form>
-					<b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-					<b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-				</b-nav-form>
-
-				<b-nav-item-dropdown text="Lang" right>
-					<b-dropdown-item href="#">EN</b-dropdown-item>
-					<b-dropdown-item href="#">ES</b-dropdown-item>
-					<b-dropdown-item href="#">RU</b-dropdown-item>
-					<b-dropdown-item href="#">FA</b-dropdown-item>
-				</b-nav-item-dropdown>
-
-				<b-nav-item-dropdown right>
-					<template v-slot:button-content>
-					<em>User</em>
-					</template>
-					<b-dropdown-item href="#">Profile</b-dropdown-item>
-					<b-dropdown-item href="#"><router-link to="/login">LOG OUT</router-link></b-dropdown-item>
-				</b-nav-item-dropdown>
-			</b-navbar-nav>
-		</b-collapse>
-		</b-navbar>
-	</div>
-</template>
-<style>
-	.listyle{
-		color : black;
-		margin : 0;	
-	}
-</style>
- -->
