c9ac2412b036fcce68a69641ad7ed6e6b9f6bdb4
westofsky <westofsky159@gmail.com>
2020-10-17 11:15:26 +0000

    Solve conflict file

--- client/src/components/SelectPage.vue
+++ client/src/components/SelectPage.vue
@@ -90,11 +90,7 @@
 			this.userdata = response.data.data.user_id+'님<br>환영합니다.'
 		}).catch((err) =>{
 			this.userdata = '로그인이 <br>필요합니다.';
-<<<<<<< HEAD
-			console.log(err);
-=======
 			console.error(err);
->>>>>>> master
 		})
 	}
   }
