83dffd8918cbdbd8cf93bfcd2bbd9a53c1f52c1e
westofsky <westofsky159@gmail.com>
2020-10-17 04:02:11 +0000

    UPDATE page components

--- client/src/components/SelectPage.vue
+++ client/src/components/SelectPage.vue
@@ -37,7 +37,6 @@
 <script>
 
   const menuList = [
-    { title: 'Home', link_to: '/'},
     { title: 'Upload', link_to: '/upload'},
     // { title: 'Test', link_to: '', sub_menu:
     //   [
@@ -91,6 +90,7 @@
 			this.userdata = response.data.data.user_id+'님<br>환영합니다.'
 		}).catch((err) =>{
 			this.userdata = '로그인이 <br>필요합니다.';
+			console.log(err);
 		})
 	}
   }
---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="dashboard">
+    <div class ="child_1">
+		<select-page/>
+		
+	</div>
+    <div class = "child_2">
+		<router-view/>	
+	</div>
+    </div>
+</template>
+
+
+<script>
+import SelectPage from '@/components/SelectPage.vue'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    SelectPage
+  }
+}
+</script>
+<style>
+body{
+	width : 100%;
+	height : 100%;
+    background-image: url(../images/background.jpg);
+    background-position: center;
+    background-size: cover;
+}
+#dashboard {
+  text-align: center;
+  display : flex;   
+}
+	.child_1{
+		flex : 1;
+		width : 10%;
+	}
+	.child_2{
+		flex : 9;
+		width : 90%;
+	}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
